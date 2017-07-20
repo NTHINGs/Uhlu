ec9af09b1777b648a267ecfc1b9aa8b0d7363ba9
Mauricio Martinez <n_othing@hotmail.com>
2017-07-20 16:45:29 -0500

    Facebook Login

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '629212693919573', // your App ID
         'clientSecret'  : 'ad3623f4772115ccd1af932aa651b4bc', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'https://uhlu.herokuapp.com/auth/facebook/callback',
         'profileFields' : ['id', 'emails', 'name']
     }
 
