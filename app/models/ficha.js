d56f5743343a3f81be7ef8c6679d0709468c4564
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-07-25 09:26:07 -0500

    Create ficha.js

---
+++ app/models/ficha.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+	//Define el modelo Ficha
+	var Ficha = sequelize.define('Ficha', {
+		nombreactividad: DataTypes.STRING,
+		seccion: DataTypes.STRING,
+		areadedesarrollo: DataTypes.STRING,
+		objetivo: DataTypes.STRING,
+		participantes: DataTypes.STRING,
+		descripcion: DataTypes.TEXT,
+		recomendaciones: DataTypes.STRING,
+		materiales: DataTypes.TEXT,
+		tiempos: DataTypes.STRING,
+		fecha: DataTypes.DATETIME,
+	}, {
+		//set the timestamps to be underscored: (created_at, updated_at)
+		underscored: true,
+		classMethods: {
+			associate: function(models) {
+				Ficha.belongsTo(models.User)
+			}
+		}
+	});
+	return Ficha;
+};
