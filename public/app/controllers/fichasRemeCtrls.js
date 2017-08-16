5435ce0cf16dbbafb21c313fa5b37b0e5970cd68
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-16 17:07:13 -0500

    Update fichasRemeCtrls.js

--- public/app/controllers/fichasRemeCtrls.js
+++ public/app/controllers/fichasRemeCtrls.js
@@ -7,50 +7,6 @@ app.controller('fichasCtrl', function($scope, $rootScope, $route, $location, $ti
 		console.log(fichas)
 	});
 
-	$scope.buscar = function(ev) {
-		Fichas.search($scope.busqueda.ficha)
-		.then(function(fichas) {
-			$scope.fichas = fichas;
-		})
-		.catch(function (error) {
-			SweetAlert.swal("Ooops..", "Ocurrio un error: "+error.data, "error");
-		});
-	};
-
-	$scope.borrar = function(ficha) {
-		var ficha = ficha;
-		SweetAlert.swal({
-			title: "Estas seguro de querer eliminar a "+ficha.nombre+"?",
-			text: "No vas a poder recuperarlo",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "#692B8D",
-			confirmButtonText: "Si, eliminalo!",
-			closeOnConfirm: false
-		},function(isConfirmed) {
-			if(isConfirmed){
-				Fichas.delete(ficha.id).then(function (s) {
-					SweetAlert.swal({
-						title: "Eliminado!",
-						text: ''+ficha.nombre+' ha sido eliminado correctamente',
-						type: "success",
-						showCancelButton: false,
-						confirmButtonColor: "#692B8D",
-						confirmButtonText: "Ok!",
-						closeOnConfirm: true
-					}, 
-					function(){ 
-						console.log("EXITO");
-						$route.reload();
-					});
-		        })
-		        .catch(function (error) {
-		        	SweetAlert.swal("Ooops..", "Ocurrio un error: "+error.data, "error");
-		        });
-			}
-		});
-	}
-
 
 	//Modal
 	$scope.showAgregarFicha = function(ev) {
@@ -135,31 +91,46 @@ app.controller('fichaCtrl',function($scope,$rootScope, $route, $location, SweetA
 		console.log($scope.ficha);
 		$rootScope.currentRoute=$scope.ficha.nombreactividad;
 	});
-	// console.log($scope.ficha);
 
-	$scope.save = function(){
-		if ($scope.ficha.myImage == 'img/fpo_avatar_multi.png') {
-			$scope.ficha.foto = 'img/fpo_avatar_multi.png';
-		}
-		// 'Exito', ''+ficha.nombre+' Fue Agregad'+s+' Correctamente'
-		$scope.ficha.user_id = $rootScope.user.id
-		Fichas.update($scope.ficha).then(function (ficha) {
-			SweetAlert.swal({
-			   title: "Exito!",
-			   text: ''+$scope.ficha.nombreactividad+' Fue Editado Correctamente',
-			   type: "success",
-			   showCancelButton: false,
-			   confirmButtonColor: "#692B8D",
-			   confirmButtonText: "Ok!",
-			   closeOnConfirm: true}, 
-			function(){ 
-				$location.path('/ficha/'+$scope.ficha.id);
-			});
-        })
-        .catch(function (error) {
-        	console.log(error)
-        	SweetAlert.swal("Ooops..", "Ocurrio un error: "+error.data, "error");
-        });
+	$scope.imprimir = function(id, $event){
+
+	};
+
+	$scope.editar = function(id, $event){
+		
+	};
+
+	$scope.eliminar = function($event){
+		SweetAlert.swal({
+			title: "Estas seguro de querer eliminar la ficha "+$scope.ficha.nombre+"?",
+			text: "No vas a poder recuperarla",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#692B8D",
+			confirmButtonText: "Si, eliminala!",
+			closeOnConfirm: false
+		},function(isConfirmed) {
+			if(isConfirmed){
+				Fichas.delete(ficha.id).then(function (s) {
+					SweetAlert.swal({
+						title: "Eliminada!",
+						text: ''+ficha.nombre+' ha sido eliminada correctamente',
+						type: "success",
+						showCancelButton: false,
+						confirmButtonColor: "#692B8D",
+						confirmButtonText: "Ok!",
+						closeOnConfirm: true
+					}, 
+					function(){ 
+						console.log("EXITO");
+						$route.reload();
+					});
+				})
+				.catch(function (error) {
+					SweetAlert.swal("Ooops..", "Ocurrio un error: "+error.data, "error");
+				});
+			}
+		});
 	};
 }); // end fichaCtrl
 
