8d18a409179b511fc344cc68cecc1f327b5d555d
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-17 16:03:54 -0500

    Update fichas.js

--- app/controllers/fichas.js
+++ app/controllers/fichas.js
@@ -1,85 +1,97 @@
 // Third Party
-var PDFDocument = require('pdfkit');
+var PdfPrinter = require('pdfmake/src/printer');
+//PdfModule
+var fichaReme = require('../pdf/fichaReme');
 // Models
 Ficha = require('../models/').Ficha;
 Users = require('../models/').User;
 // CRUD Operations for Ficha Model
-module.exports= {
-  index(req, res) {
-    Ficha.findAll()
-      .then(function (Fichas) {
-        res.status(200).json(Fichas);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.status(500).json(error);
-      });
-  },
+module.exports = {
+    index(req, res) {
+        Ficha.findAll()
+            .then(function (Fichas) {
+                res.status(200).json(Fichas);
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).json(error);
+            });
+    },
 
-  show(req, res) {
-    Ficha.findAll({
-      where: {
-        id: req.params.id
-      },
-      include: [ {model: Users} ]
-    })
-    .then(function (Ficha) {
-      res.status(200).json(Ficha);
-    })
-    .catch(function (error){
-      console.log(error);
-      res.status(500).json(error);
-    });
-  },
+    show(req, res) {
+        Ficha.findAll({
+            where: {
+                id: req.params.id
+            },
+            include: [{ model: Users }]
+        })
+            .then(function (Ficha) {
+                res.status(200).json(Ficha);
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).json(error);
+            });
+    },
 
-  create(req, res) {
-    // res.status(200).json(req.body);
-    Ficha.create(req.body)
-      .then(function (newFicha) {
-        res.status(200).json(newFicha);
-      })
-      .catch(function (error){
-        res.status(500).json(error);
-      });
-  },
+    create(req, res) {
+        // res.status(200).json(req.body);
+        Ficha.create(req.body)
+            .then(function (newFicha) {
+                res.status(200).json(newFicha);
+            })
+            .catch(function (error) {
+                res.status(500).json(error);
+            });
+    },
 
-  update(req, res) {
-    Ficha.update(req.body, {
-      where: {
-        id: req.body.id
-      }
-    })
-    .then(function (updatedRecords) {
-      res.status(200).json(updatedRecords);
-    })
-    .catch(function (error){
-      console.log(error);
-      res.status(500).json(error);
-    });
-  },
+    update(req, res) {
+        Ficha.update(req.body, {
+            where: {
+                id: req.body.id
+            }
+        })
+            .then(function (updatedRecords) {
+                res.status(200).json(updatedRecords);
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).json(error);
+            });
+    },
 
-  delete(req, res) {
-    console.log(req.params)
-    Ficha.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-    .then(function (deletedRecords) {
-      res.status(200).json(deletedRecords);
-    })
-    .catch(function (error){
-      console.log(error);
-      res.status(500).json(error);
-    });
-  },
-  
-  print(req, res) {
-    doc = new PDFDocument();
-    // Adding PDF Content
-    doc.text("Hola!");
-    doc.pipe(res);
-    doc.end();     
-    // req.body
-  }
+    delete(req, res) {
+        console.log(req.params)
+        Ficha.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(function (deletedRecords) {
+                res.status(200).json(deletedRecords);
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).json(error);
+            });
+    },
+
+    print(req, res) {
+        Ficha.findAll({
+            where: {
+                id: req.params.id
+            },
+            include: [{ model: Users }]
+        })
+            .then(function (Ficha) {
+                var doc = fichaReme.pdf(Ficha);
+                doc.pipe(res);
+                doc.end();
+                // req.body
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).json(error);
+            });
+    }
 };
