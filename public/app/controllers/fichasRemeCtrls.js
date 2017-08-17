b3c4a727b53eee697be80c696cf29c664d29304b
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-17 13:17:05 -0500

    Update fichasRemeCtrls.js

--- public/app/controllers/fichasRemeCtrls.js
+++ public/app/controllers/fichasRemeCtrls.js
@@ -100,10 +100,71 @@ app.controller('fichaCtrl',function($scope,$rootScope, $route, $location, SweetA
 	$scope.imprimir = function(id, $event){
 
 	};
+	//Modal
+	$scope.editar = function(ev) {
+		$mdDialog.show({
+			controller: function($scope){
+				// Modal Actions
+				$scope.cancel = function(){
+					$mdDialog.cancel(); 
+				};
 
-	$scope.editar = function(id, $event){
+				$scope.save = function(){
+					$mdDialog.hide($scope.ficha);	
+				};
 
-	};
+				$scope.add = function($event){
+					$event.preventDefault();
+				   	$event.stopPropagation();
+					$scope.ficha.materiales.push({material: ''});
+				}
+
+				$scope.del = function(i, $event){
+					$event.preventDefault();
+				   	$event.stopPropagation();
+					$scope.ficha.materiales.splice(i,1);
+				}
+			},
+			templateUrl: '/dialogs/agregarFichaReme.html',
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose:true,
+			fullscreen: 'false' // Only for -xs, -sm breakpoints.
+		})
+		.then(function(ficha) {
+			ficha.seccion = $rootScope.user.seccion;
+			var materiales = "";
+			ficha.materiales.forEach(function(material) {
+				materiales += material.material +'<br>';
+			});
+			ficha.materiales = materiales;
+			ficha.autor = $rootScope.user.id;
+
+			console.log(ficha);
+			Fichas.update(ficha).then(function (ficha) {
+				SweetAlert.swal({
+					title: "Exito!",
+					text: ''+ficha.nombreactividad+' Fue Agregado Correctamente',
+					type: "success",
+					showCancelButton: false,
+					confirmButtonColor: "#692B8D",
+					confirmButtonText: "Ok!",
+					closeOnConfirm: true
+				}, 
+				function(){ 
+					console.log("EXITO");
+					$route.reload();
+				});
+	        })
+	        .catch(function (error) {
+	        	console.log(error);
+	        	SweetAlert.swal("Ooops..", "Ocurrio un error: "+error.data, "error");
+	        });
+		}, function() {
+			
+		});
+		
+	};//end modal
 
 	$scope.eliminar = function($event){
 		SweetAlert.swal({
