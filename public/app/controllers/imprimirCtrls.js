08aa3d5b2d99ea1d58e7da37b4306dc8b2bc56b9
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-21 17:35:26 -0500

    Update imprimirCtrls.js

--- public/app/controllers/imprimirCtrls.js
+++ public/app/controllers/imprimirCtrls.js
@@ -4,6 +4,11 @@ app.controller('imprimirCtrl', function($scope, $rootScope, $mdDialog, Patrullas
     Patrullas.all($rootScope.user.id).then(function(patrullas) {
         $scope.patrullas = patrullas;
     });
+
+    $scope.generarReporte = function(tipo){
+        console.log(tipo);
+    }
+
 	$scope.cancel = function(){
         $mdDialog.cancel(); 
     };
