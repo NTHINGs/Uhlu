55513da50fa9c293ad99d0f0717b306ca519aa6a
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-21 17:25:37 -0500

    Update imprimirCtrls.js

--- public/app/controllers/imprimirCtrls.js
+++ public/app/controllers/imprimirCtrls.js
@@ -1,3 +1,6 @@
 app.controller('imprimirCtrl', function($scope, $rootScope, $http, $location, Config){
-	
+    $scope.patrullaClicked = false;
+	$scope.cancel = function(){
+        $mdDialog.cancel(); 
+    };
 });
