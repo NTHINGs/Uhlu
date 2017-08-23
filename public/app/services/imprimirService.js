908788f4bb63325d3c225a8ccd7350fb205c74f7
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-23 17:05:10 -0500

    Update imprimirService.js

--- public/app/services/imprimirService.js
+++ public/app/services/imprimirService.js
@@ -1,7 +1,11 @@
 app.service('Imprimir', function($http, $location) {
     return {
         generarReporte: function(tipo, print){
-            $location.path('/generarReporte/' + tipo + '/' + print.opcion + '/' + print.patrulla)
+            var patrulla = '';
+            if(typeof(print.patrulla) != 'undefined'){
+                patrulla = '&patrulla' + print.patrulla;
+            }
+            $location.path('/generarReporte?tipo=' + tipo + '&opcion=' + print.opcion + patrulla)
         }
     }
   });
