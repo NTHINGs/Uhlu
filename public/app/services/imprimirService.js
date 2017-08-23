b69d59817e490eda460f823e362082c57d524ee1
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-23 16:26:14 -0500

    Update imprimirService.js

--- public/app/services/imprimirService.js
+++ public/app/services/imprimirService.js
@@ -9,7 +9,9 @@ app.service('Imprimir', function($http, $q) {
             $http
             .get('/generarReporte', body)
             .then(function (response) {
-                deferred.resolve(response.data);
+                var file = new Blob([data], {type: 'application/pdf'});
+                var fileURL = URL.createObjectURL(file);
+                window.open(fileURL);
             })
             .catch(function (error) {
                 deferred.reject(error);
