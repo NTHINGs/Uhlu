0933171f22ad2767a900993cf9e7d0ab13e32f91
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-23 16:04:59 -0500

    Update generarReporte.js

--- app/pdf/generarReporte.js
+++ app/pdf/generarReporte.js
@@ -12,99 +12,99 @@ var fontDescriptors = {
 module.exports = {
     generarDirectorio(Scouts) {
         var rows = [];
-        Scouts.forEach(function(scout){
+        for(var i = 0; i < Scouts.length; i++){
             // 'FOTO' 'CUM' 'NOMBRE' 'GÉNERO' 'FECHA DE NACIMIENTO' 'ESCUELA' 'E-MAIL' 'TELÉFONO' 'DOMICILIO'
             rows.push([ 
                 {
-                    image: scout.dataValues.foto,
+                    image: Scouts[i].dataValues.foto,
                     width: 150,
                     height: 150
                 }, 
-                { text: scout.dataValues.cum},
-                { text: scout.dataValues.nombre},
-                { text: scout.dataValues.genero},
-                { text: scout.dataValues.fechanacimiento},
-                { text: scout.dataValues.escuela},
-                { text: scout.dataValues.email},
-                { text: scout.dataValues.telefono},
-                { text: scout.dataValues.domicilio},
+                { text: Scouts[i].dataValues.cum},
+                { text: Scouts[i].dataValues.nombre},
+                { text: Scouts[i].dataValues.genero},
+                { text: Scouts[i].dataValues.fechanacimiento},
+                { text: Scouts[i].dataValues.escuela},
+                { text: Scouts[i].dataValues.email},
+                { text: Scouts[i].dataValues.telefono},
+                { text: Scouts[i].dataValues.domicilio},
             ]);
-            // PDF Content
-            var dd= {
-                info: {
-                    title: 'Directorio',
-                    author: 'Uhlu',
-                    subject: 'Directorio',
-                    keywords: 'scouts',
-                    creator: 'http://uhluscout.com'
-                },
-                content: [
-                    {
-                        layout: 'noBorders',
-                        table: {
-                            headerRows: 0,
-                            widths: [ '*', '*'],
-                            
-                            body: [
-                                [ { image: path.join(__dirname, '/../../public/img/logo.png'), width: 100,height: 100}, { text: 'Directorio', style: 'header'}]
-                            ]
-                        },
-                        style: 'marginBot'
-                    },
-                    {
-                        layout: 'headerLineOnly',
-                        table: {
-                            headerRows: 1,
-                            widths: [ 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', '*' ],
-                            // cum: DataTypes.STRING, 
-                            // foto: DataTypes.TEXT,
-                            // nombre: DataTypes.STRING,
-                            // genero: DataTypes.STRING,
-                            // fechanacimiento: DataTypes.DATE,
-                            // escuela: DataTypes.STRING,
-                            // email: DataTypes.STRING,
-                            // telefono: DataTypes.STRING,
-                            // domicilio: DataTypes.STRING,
-                            body: [
-                                [ 
-                                    { text: 'FOTO', style: 'morrarro' }, 
-                                    { text: 'CUM', style: 'morrarro' }, 
-                                    { text: 'NOMBRE', style: 'morrarro' }, 
-                                    { text: 'GÉNERO', style: 'morrarro' }, 
-                                    { text: 'FECHA DE NACIMIENTO', style: 'morrarro' }, 
-                                    { text: 'ESCUELA', style: 'morrarro' }, 
-                                    { text: 'E-MAIL', style: 'morrarro' },
-                                    { text: 'TELÉFONO', style: 'morrarro' }, 
-                                    { text: 'DOMICILIO', style: 'morrarro' },
-                                ],
-                                rows
-                            ]
-                        },
-                        style: 'marginBot'
-                    },
-                    { text: 'Directorio elaborado desde http://uhluscout.com', link: 'http://uhluscout.com', style: 'footer'}  
-                ],
-                styles: {
-                    header: {
-                        fontSize: 22,
-                        bold: true,
-                        alignment: 'right',
-                        margin: [ 0, 0, 0, 25 ],
-                        color: 'purple'
-                    },
-                    marginBot:{
-                        margin: [ 0, 0, 0, 25 ]
-                    },
-                    morrarro:{
-                        color: 'purple'
-                    },
-                    footer: {
-                        bold: true,
-                        alignment: 'right'
-                    },
-                }
-            };//end dd
         });
+        // PDF Content
+        var dd= {
+            info: {
+                title: 'Directorio',
+                author: 'Uhlu',
+                subject: 'Directorio',
+                keywords: 'scouts',
+                creator: 'http://uhluscout.com'
+            },
+            content: [
+                {
+                    layout: 'noBorders',
+                    table: {
+                        headerRows: 0,
+                        widths: [ '*', '*'],
+                        
+                        body: [
+                            [ { image: path.join(__dirname, '/../../public/img/logo.png'), width: 100,height: 100}, { text: 'Directorio', style: 'header'}]
+                        ]
+                    },
+                    style: 'marginBot'
+                },
+                {
+                    layout: 'headerLineOnly',
+                    table: {
+                        headerRows: 1,
+                        widths: [ 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', '*' ],
+                        // cum: DataTypes.STRING, 
+                        // foto: DataTypes.TEXT,
+                        // nombre: DataTypes.STRING,
+                        // genero: DataTypes.STRING,
+                        // fechanacimiento: DataTypes.DATE,
+                        // escuela: DataTypes.STRING,
+                        // email: DataTypes.STRING,
+                        // telefono: DataTypes.STRING,
+                        // domicilio: DataTypes.STRING,
+                        body: [
+                            [ 
+                                { text: 'FOTO', style: 'morrarro' }, 
+                                { text: 'CUM', style: 'morrarro' }, 
+                                { text: 'NOMBRE', style: 'morrarro' }, 
+                                { text: 'GÉNERO', style: 'morrarro' }, 
+                                { text: 'FECHA DE NACIMIENTO', style: 'morrarro' }, 
+                                { text: 'ESCUELA', style: 'morrarro' }, 
+                                { text: 'E-MAIL', style: 'morrarro' },
+                                { text: 'TELÉFONO', style: 'morrarro' }, 
+                                { text: 'DOMICILIO', style: 'morrarro' },
+                            ],
+                            rows
+                        ]
+                    },
+                    style: 'marginBot'
+                },
+                { text: 'Directorio elaborado desde http://uhluscout.com', link: 'http://uhluscout.com', style: 'footer'}  
+            ],
+            styles: {
+                header: {
+                    fontSize: 22,
+                    bold: true,
+                    alignment: 'right',
+                    margin: [ 0, 0, 0, 25 ],
+                    color: 'purple'
+                },
+                marginBot:{
+                    margin: [ 0, 0, 0, 25 ]
+                },
+                morrarro:{
+                    color: 'purple'
+                },
+                footer: {
+                    bold: true,
+                    alignment: 'right'
+                },
+            }
+        };//end dd
 
 
         return new PdfPrinter(fontDescriptors).createPdfKitDocument(dd);
