7c56303b7949dcb11d0135abbea447bced036ed8
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-25 15:52:03 -0500

    Update generarReporte.js

--- app/pdf/generarReporte.js
+++ app/pdf/generarReporte.js
@@ -38,7 +38,7 @@ module.exports = {
                 }, 
                 { text: Scouts[i].dataValues.cum},
                 { text: Scouts[i].dataValues.nombre},
-                { text: Scouts[i].dataValues.Patrulla.nombre},
+                { text: Scouts[i].dataValues.Patrulla.dataValues.nombre},
                 { text: Scouts[i].dataValues.genero},
                 { text: '' + Scouts[i].dataValues.fechanacimiento.getDate() + '/' + (Scouts[i].dataValues.fechanacimiento.getMonth() + 1) + '/' + Scouts[i].dataValues.fechanacimiento.getFullYear() + ''},
                 { text: Scouts[i].dataValues.escuela},
