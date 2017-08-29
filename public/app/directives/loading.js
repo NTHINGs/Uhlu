ea0268a137652abce396b07a556c4bd4b268050b
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-29 13:48:20 -0500

    Update loading.js

--- public/app/directives/loading.js
+++ public/app/directives/loading.js
@@ -3,18 +3,17 @@ app.directive('loading', ['$http' ,function ($http)
     return {  
         restrict: 'A',  
         template: '<div class="loading-spiner"><img src="img/loading.gif" /> </div>',  
-        link: function (scope, elm, attrs)  
-        {  
+        link: function (scope, elm, attrs){  
             scope.isLoading = function () {  
                 return $http.pendingRequests.length > 0;  
             };  
  
-            scope.$watch(scope.isLoading, function (v)  
-            {  
+            scope.$watch(scope.isLoading, function (v){  
+                console.log(elm);
                 if(v){  
-                    elm.style.display = 'block';  
+                    elm.show();  
                 }else{  
-                    elm.style.display = 'none'; 
+                    elm.hide();  
                 }  
             });  
         }  
