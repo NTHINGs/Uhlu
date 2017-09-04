4d270c6d37c2ad621d7a42d6776516fd76a6e627
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-09-04 13:48:40 -0500

    Update auth.js

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,9 +1,35 @@
 module.exports = {
-
-    'facebookAuth' : {
-        'clientID'      : '629212693919573', // your App ID
-        'clientSecret'  : 'ad3623f4772115ccd1af932aa651b4bc', // your App Secret
-        'profileFields' : ['id', 'emails', 'name']
+    'development':{
+        'facebookAuth' : {
+            'clientID'      : '629212693919573', // your App ID
+            'clientSecret'  : 'ad3623f4772115ccd1af932aa651b4bc', // your App Secret
+            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+            'profileFields' : ['id', 'emails', 'name']
+        }
+    },
+    'test':{
+        'facebookAuth' : {
+            'clientID'      : '629212693919573', // your App ID
+            'clientSecret'  : 'ad3623f4772115ccd1af932aa651b4bc', // your App Secret
+            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+            'profileFields' : ['id', 'emails', 'name']
+        }
+    },
+    'staging':{
+        'facebookAuth' : {
+            'clientID'      : '629212693919573', // your App ID
+            'clientSecret'  : 'ad3623f4772115ccd1af932aa651b4bc', // your App Secret
+            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+            'profileFields' : ['id', 'emails', 'name']
+        }
+    },
+    'production':{
+        'facebookAuth' : {
+            'clientID'      : '629212693919573', // your App ID
+            'clientSecret'  : 'ad3623f4772115ccd1af932aa651b4bc', // your App Secret
+            'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+            'profileFields' : ['id', 'emails', 'name']
+        }
     }
 
 };
