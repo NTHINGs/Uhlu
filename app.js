de09163ba390b157736810b9ec88a93ca134784e
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-09-08 17:03:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(flash());
 app.use(function(req, res, next){
     res.locals.message = req.flash('message');
     res.locals.success = req.flash('success');
-    req.flash();
+    console.log(res.locals);
     next();
 });
 
