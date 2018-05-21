917d0e988a00673e05a1c8ecfc3c4285048e05f3
Mauricio Martinez <n_othing@hotmail.com>
2018-05-21 09:51:38 -0500

    Travis Play

--- gcloud-sdk-install.sh
+++ gcloud-sdk-install.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-curl https://sdk.cloud.google.com/ | bash
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
