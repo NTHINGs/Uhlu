59ff08cc8471171d7a900e97d09457f3e19d94e2
Mauricio Martinez <n_othing@hotmail.com>
2018-05-21 10:00:34 -0500

    Travis Play

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-if [ ! -f "$HOME/google-cloud-sdk/bin/kubectl" ] then
-    gcloud --quiet components update kubectl
-    gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
-    gcloud config set project $PROJECT_ID
-    gcloud config set compute/zone $ZONE
+if [ ! -f "$HOME/google-cloud-sdk/bin/kubectl" ]; then 
+    gcloud --quiet components update kubectl;
+    gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}";
+    gcloud config set project $PROJECT_ID;
+    gcloud config set compute/zone $ZONE;
 fi
