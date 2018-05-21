7fb3cd4fd2cca2081bc6f0a0685564ff157a87b5
Mauricio Martinez <n_othing@hotmail.com>
2018-05-21 10:53:56 -0500

    Travis Play

--- image-prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-#Build image
-docker build --no-cache=true -t $1 .
--- k8s-run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-kubectl create -f deployment.yml
-kubectl create -f service.yml
